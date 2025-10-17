feb0d57eea34d01a25f69dea45367952c408d912
Mateus Oliveira <mateusoliveiradeveloper1@gmail.com>
2025-10-17 13:41:41 -0400

    Added WebMvcConfigurer

---
+++ src/main/java/br/com/mateus/springboot2_essentials/configurer/MyWebMvcConfigurer.java
@@ -0,0 +1,20 @@
+package br.com.mateus.springboot2_essentials.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableArgumentResolver;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+        pageHandler.setFallbackPageable(PageRequest.of(1,5));
+        resolvers.add(pageHandler);
+    }
+}
